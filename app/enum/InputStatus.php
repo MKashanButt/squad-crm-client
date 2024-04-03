df6e56a7958009155369925581e9a6202af53afa
MKashanButt <kashanbutt566@gmail.com>
2024-04-03 23:39:32 +0500

    updated enum

--- app/enum/InputStatus.php
+++ app/enum/InputStatus.php
@@ -14,8 +14,8 @@ enum InputStatus: string implements HasLabel, HasColor
     case WRONG_DOC = 'Wrong doc';
     case PAID = 'Paid';
     case AWAITING = 'Awaiting';
-    case TRANSFERED = 'Transfered';
-    case NOT_TRANSFERED = 'Not transfered';
+    case TRANSFERRED = 'Transferred';
+    case NOT_TRANSFERRED = 'Not transferred';
 
     public function getLabel(): ?string
     {
@@ -27,8 +27,8 @@ enum InputStatus: string implements HasLabel, HasColor
             self::WRONG_DOC => 'Wrong doc',
             self::PAID => 'Paid',
             self::AWAITING => 'Awaiting',
-            self::TRANSFERED => 'Transfered',
-            self::NOT_TRANSFERED => 'Not transfered',
+            self::TRANSFERRED => 'Transferred',
+            self::NOT_TRANSFERRED => 'Not transferred',
         };
     }
     public function getColor(): string|array|null
@@ -40,9 +40,9 @@ enum InputStatus: string implements HasLabel, HasColor
             self::APPROVED => 'success',
             self::WRONG_DOC => 'pink',
             self::PAID => 'cyan',
+            self::TRANSFERRED => 'primary',
+            self::NOT_TRANSFERRED => 'danger',
             self::AWAITING => '',
-            self::TRANSFERED => 'primary',
-            self::NOT_TRANSFERED => 'danger',
         };
     }
 }
